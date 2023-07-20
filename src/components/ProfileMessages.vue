f00d9514c13575bd7981e36c3d2ac3c190aa4f82
Alexmust2 <alex.must2003@mail.ru>
2023-07-20 16:28:52 +0300

    fix

--- src/components/ProfileMessages.vue
+++ src/components/ProfileMessages.vue
@@ -29,11 +29,4 @@ export default {
             font-size: 12px;
         }
     }
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity .5s ease;
-    }
-
-    .fade-enter-from, .fade-leave-to {
-        opacity: 0;
-    }
 </style>
