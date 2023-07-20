32af5d1f44a6f156a0cf2813369a92e09662a6bd
Alexmust2 <alex.must2003@mail.ru>
2023-07-20 16:29:26 +0300

    real time fetch status, delete & add status

--- src/mixins/UserStatus.js
+++ src/mixins/UserStatus.js
@@ -1,25 +1,69 @@
+import { db } from '@/main'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { addDoc, collection, doc, deleteDoc, onSnapshot, orderBy, query} from 'firebase/firestore'
+
+
 export default {
     data() {
         return {
             statusList: [
-                {body: "Всем привет, я тут новенький, поставьте мне лайк пожалуйста", date: Date()},
-                {body: "Чем у вас тут можно заниматься?", date: Date()}
             ],
             statusBody: '',
-            image: require('@/assets/logo.png')
+            image: require('@/assets/logo.png'),
         }
     },
+    mounted() {
+        this.getStatusList()
+    },
     methods: {
+        async getStatusList() {
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    const getUser = getAuth().currentUser.email
+                    const statusRef = collection(db, getUser)
+                    const listQueue = query(statusRef, orderBy('date', 'desc'))
+                    onSnapshot(listQueue, (snapshot) => {
+                        snapshot.docs.forEach((doc) => {
+                            const status = []
+                            status.push({
+                                'id': doc.id,
+                                'body': doc.data().body,
+                                'date': doc.data().date
+                            })
+                            this.statusList.push(...status)
+                        })
+                    })
+                }
+                else {
+                    console.log('loggetout')
+                }
+            });
+        },
         addStatus() {
-            this.statusList.push({
-                body: this.statusBody,
-                date: Date()
-            })
-            this.statusBody = ''
-            console.log('successeful')
+            const getUser = getAuth().currentUser.email
+            const id = Object.keys(this.statusList).length
+            const statusRef = collection(db, getUser )
+            const date = new Date()
+            if (this.statusBody != ''){
+                addDoc(statusRef, {
+                    body: this.statusBody,
+                    date: date.toLocaleString('ru'),
+                })
+                this.statusBody = ''
+                this.statusList = []
+            } else {
+                alert("Заполните поле статуса")
+            }
         },
         changePhoto() {
             this.image = require('@/assets/ava.png')
+        },
+        deleteStatus(id) {
+            const getUser = getAuth().currentUser.email
+            const statusRef = collection(db, getUser)
+            deleteDoc(doc(statusRef, id))
+            this.statusList = []
         }
-    },
+    }
 }
